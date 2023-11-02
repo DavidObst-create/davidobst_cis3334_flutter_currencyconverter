94ff4488ff1047b180d68a55370db46d0568ed90
jobst <jobst@css.edu>
2023-11-02 11:21:36 -0500

    Initial Commit and Push to create repository. Actual submission will be in another push

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,11 @@ class MyHomePage extends StatefulWidget {
 //Code is entered here
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final dollarTextField = TextEditingController();
+  final euroTextField = TextEditingController();
+  double _dollars = 0;
+  double _euros = 0;
+  double _conversionRate = 1.06;
 
   void _incrementCounter() {
     setState(() {
@@ -54,26 +59,36 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
 
-
-
-            Text('Dollars',),
-            TextField(),
+            TextField(controller: dollarTextField, keyboardType: TextInputType.number),
+            Text('Dollars'),
             ElevatedButton(
-              child: Text("Convert"),
+              child: Text("Convert to Euros"),
               onPressed: (){
-                print("Convert button pressed");
+                print("Convert to Euros button pressed");
+                _dollars = double.parse(dollarTextField.text);
+                _euros = _dollars / _conversionRate;
+                euroTextField.text = _euros.toStringAsFixed(2);
+                print(_euros);
+              },
+            ),
+
+            TextField(controller: euroTextField, keyboardType: TextInputType.number),
+            Text('Euros'),
+            ElevatedButton(
+              child: Text("Convert to Dollars"),
+              onPressed: (){
+                print("Convert to Dollars button pressed");
+                _euros = double.parse(euroTextField.text);
+                _dollars = _euros * _conversionRate;
+                dollarTextField.text = _dollars.toStringAsFixed(2);
+                print(_dollars);
               },
             )
-
-
           ],
+
+
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
